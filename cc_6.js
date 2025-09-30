5c91bf1ac8256d725e6a685c6463bca70241cab9
Natalie Danner <danner15@usf.edu>
2025-09-30 12:41:30 -0400

    Created sample employees and manager

--- cc_6.js
+++ cc_6.js
@@ -19,10 +19,17 @@ class Manager extends Employee{
         this.teamSize = teamSize
     }
     describe(){
-        return `${this.name} works in the ${this.department} and manages ${this.teamSize} people.`
+        return `${this.name} works in the ${this.department} department and manages ${this.teamSize} people.`
     }
 }
 
-// Create sample employees
+// Create sample employees and manager
 
+let employees = [
+    new Employee("Alice", "Finance"),
+    new Employee("Jim", "Advertising"),
+    new Employee("Amanda", "Sales")
+];
+
+let manager = new Manager("Taylor", "HR", 8);
 
